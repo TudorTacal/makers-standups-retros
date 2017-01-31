a77d8e3031e116e05ac93eb3fb965e9525eafdd2
ajbater <amanda22@hotmail.co.uk>
2017-01-31 16:36:19 +0000

    Tried to render React components with Express routes

---
+++ src/views/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import landingPage from '../views/landingPage.jsx';
+
+ReactDOM.render(
+  < landingPage title = 'Standups and Retros' />,
+  document.getElementById('root')
+);
