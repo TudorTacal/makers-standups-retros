841cde064cbceaa8c19c242cd4360670b3fe662f
ajbater <amanda22@hotmail.co.uk>
2017-01-31 16:41:32 +0000

    WIP to render a React Component through Express routing

--- src/server.js
+++ src/server.js
@@ -1,8 +1,15 @@
 var express = require('express');
+
 var app = express();
+
+app.engine('jsx', require('express-react-views').createEngine());
+
+app.set('views', './views');
+app.set('view engine', 'jsx');
 
 app.get('/', function (req,res) {
   res.status(200).send('ok')
+  res.render('landingPage', { title: 'Standups and Retros' });
 })
 
 var server = app.listen(3001, function () {
