691ec1a10e5368e629299d8d58631b81b4f68688
TudorTacal <tudortacal@gmail.com>
2017-02-01 14:50:00 +0000

    Add an image with a plus sign. Saves the clicks in the item state. Increases the clicks number and displays it when the user click on the + picture.WIP to start the react app in the browser.

--- src/Item.js
+++ src/Item.js
@@ -1,9 +1,21 @@
 import React, { Component } from 'react';
 
 class Item extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clicks: 0
+    };
+  }
+  onPlusClick(event) {
+    this.setState({
+      clicks: this.state.clicks + 1
+    })
+  }
   render(){
+    let image = <img src="/images/plus.png" onClick={this.onPlusClick} alt="Plus" height="20" width="20"/>
     return (
-      <li className="item">{this.props.text}</li>
+      <li className="item" >{this.props.text}{image}{this.state.clicks}</li>
     );
   }
 };
---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Standup from './components/StandupPage';
+
+
+ReactDOM.render(
+  <StandupPage />,
+  document.getElementById('root')
+);
