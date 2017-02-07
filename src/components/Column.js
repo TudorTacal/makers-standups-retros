7df7b007c3e9825637416e7bc7b38bf0b8f7e379
Kim Wilson <k.wilson541@gmail.com>
2017-02-07 15:22:47 +0000

    Aligned item text to left of column and item counters to right of column

--- src/components/Column.js
+++ src/components/Column.js
@@ -7,9 +7,7 @@ class Column extends Component {
 		return (
 			<div className="column">
 				<h1>{ this.props.title }</h1>
-				<div className="column-content">
-					<ItemList id={this.props.id} title={this.props.title}/>
-				</div>
+				<ItemList id={this.props.id} title={this.props.title}/>
 			</div>
 		);
 	}
