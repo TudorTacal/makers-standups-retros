07e693dbe2ec34425531c7863a4a26fc56fed827
TudorTacal <tudortacal@gmail.com>
2017-02-08 10:29:07 +0000

    Add the mongo url

--- src/server.js
+++ src/server.js
@@ -14,7 +14,7 @@ import mongoose from 'mongoose'
 import MongoItem from './models/mongoItem'
 
 
-var url = MONGOLAB_URI
+var url = "mongodb://tudor:Standups10!@ds035059.mlab.com:35059/standups"
 mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
