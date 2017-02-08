14c6acc31b8937f96b85b3146f2af539bb9a78af
TudorTacal <tudortacal@gmail.com>
2017-02-08 17:52:19 +0000

    Fixes mongodb variable name

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ import mongoose from 'mongoose'
 import MongoItem from './models/mongoItem'
 
 
-var url = MONGO_URI
+var url = MONGOLAB_URI
 mongoose.connect(url);
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error: '));
