4b3cc31c49f28e61e841c8968bbe2a6dfa1f7dd7
Kim Wilson <k.wilson541@gmail.com>
2017-02-08 17:23:04 +0000

    Refactored RetroPage tests to also expect relevant child componenets to be rendered

--- test/RetroPage.spec.js
+++ test/RetroPage.spec.js
@@ -3,6 +3,9 @@ import sinon from 'sinon';
 import { mount, shallow } from 'enzyme';
 import { expect } from 'chai';
 import RetroPage from '../src/components/RetroPage.js'
+import Board from '../src/components/Board.js'
+import UserInfo from '../src/components/UserInfo.js'
+import Chat from '../src/components/Chat.js'
 
 describe('<RetroPage/>', function() {
 
@@ -15,6 +18,7 @@ describe('<RetroPage/>', function() {
   it('should render a board', () => {
 	const wrapper = mount(<RetroPage/>);
 	expect(wrapper.find('div.board')).to.have.length(1);
+    expect(wrapper.find(Board)).to.have.length(1);
   });
 
   it('should render a sidebar', () => {
@@ -25,11 +29,13 @@ describe('<RetroPage/>', function() {
   it('should render the userInfo', () => {
 	const wrapper = mount(<RetroPage/>);
 	expect(wrapper.find('div.userInfo')).to.have.length(1);
+    expect(wrapper.find(UserInfo)).to.have.length(1);
   });
 
   it('should render the chat', () => {
 	const wrapper = mount(<RetroPage/>);
 	expect(wrapper.find('div.chat')).to.have.length(1);
+    expect(wrapper.find(Chat)).to.have.length(1);
   });
 
 });
