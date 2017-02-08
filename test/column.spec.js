29975965a2d7ac482eb55927bf4e818a4d6d6e0e
ajbater <amanda22@hotmail.co.uk>
2017-02-08 20:30:01 +0000

    test for column - renders with correct props

--- test/column.spec.js
+++ test/column.spec.js
@@ -10,4 +10,11 @@ describe("Column />", () => {
     const wrapper = shallow(<Column />);
     expect(wrapper).to.have.length(1);
   })
+
+  it("has 'type', 'title' and 'id' as props", () => {
+    const wrapper = mount(<Column type="standup" title="Learnt" id="123" />);
+    expect(wrapper.props().type).to.equal("standup");
+    expect(wrapper.props().title).to.equal("Learnt");
+    expect(wrapper.props().id).to.equal("123");
+  })
 });
