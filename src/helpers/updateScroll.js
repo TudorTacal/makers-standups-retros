adfa630589be3c735aebb49afcb32e2e77f07857
ajbater <amanda22@hotmail.co.uk>
2017-02-10 13:50:57 +0000

    fixes css bug

--- src/helpers/updateScroll.js
+++ src/helpers/updateScroll.js
@@ -1,8 +1,6 @@
 var updateScroll = function(elementClass) {
-  console.log(elementClass);
   var objDiv = document.getElementsByClassName(elementClass)[0];
   objDiv.scrollTop = objDiv.scrollHeight;
-  console.log('this gets called')
 };
 
 export default updateScroll;
